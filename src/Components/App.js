ba55240f4855ab3c64a0a8bff266a6589f52dfe4
DuanLeeWatkins <leeduan19@gmail.com>
2023-05-25 21:58:15 -0400

    updated Create link component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
 
 import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
 
 function App() {
   return (
     <div className="App">
-      <LinkList />
+     <CreateLink />
     </div>
   );
 }
